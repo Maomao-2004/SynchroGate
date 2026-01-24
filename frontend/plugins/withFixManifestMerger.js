a25fa481b45938580a0d7ae57348733bc1b606bb
Maomao-2004 <20palabayronhiel04@gmail.com>
2026-01-25 01:54:25 +0800

    Remove notification color config to fix manifest merger conflict

--- frontend/plugins/withFixManifestMerger.js
+++ frontend/plugins/withFixManifestMerger.js
@@ -3,28 +3,44 @@ const { withAndroidManifest } = require('@expo/config-plugins');
 /**
  * Fix manifest merger conflict between expo-notifications and @react-native-firebase/messaging
  * Both try to set com.google.firebase.messaging.default_notification_color
+ * This plugin adds tools:replace to allow our value to override
  */
 const withFixManifestMerger = (config) => {
   return withAndroidManifest(config, async (config) => {
     const androidManifest = config.modResults;
-    const mainApplication = androidManifest.manifest.application[0];
 
-    // Find and fix the notification color meta-data
-    if (mainApplication['meta-data']) {
-      mainApplication['meta-data'] = mainApplication['meta-data'].map((metaData) => {
-        if (
-          metaData.$['android:name'] === 'com.google.firebase.messaging.default_notification_color'
-        ) {
-          // Add tools:replace to fix the conflict
-          metaData.$['tools:replace'] = 'android:resource';
-        }
-        return metaData;
-      });
-    }
-
-    // Ensure tools namespace is declared
+    // Ensure tools namespace is declared in manifest root
     if (!androidManifest.manifest.$['xmlns:tools']) {
       androidManifest.manifest.$['xmlns:tools'] = 'http://schemas.android.com/tools';
+    }
+
+    const mainApplication = androidManifest.manifest.application?.[0];
+    if (!mainApplication) {
+      return config;
+    }
+
+    // Find and fix the notification color meta-data entries
+    if (mainApplication['meta-data']) {
+      mainApplication['meta-data'] = mainApplication['meta-data'].map((metaData) => {
+        const name = metaData.$['android:name'];
+        
+        // Fix notification color conflict
+        if (name === 'com.google.firebase.messaging.default_notification_color') {
+          metaData.$['tools:replace'] = 'android:resource';
+        }
+        
+        // Fix notification icon conflict if it exists
+        if (name === 'com.google.firebase.messaging.default_notification_icon') {
+          metaData.$['tools:replace'] = 'android:resource';
+        }
+        
+        // Fix notification channel id conflict if it exists
+        if (name === 'com.google.firebase.messaging.default_notification_channel_id') {
+          metaData.$['tools:replace'] = 'android:value';
+        }
+        
+        return metaData;
+      });
     }
 
     return config;
@@ -32,4 +48,3 @@ const withFixManifestMerger = (config) => {
 };
 
 module.exports = withFixManifestMerger;
-
